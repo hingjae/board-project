6f22b5e5fc474e89e71feb964ff9f4fee6ea5cdc
hingjae <ljh23270@naver.com>
2023-04-20 17:09:28 +0900

    #8 - 도메인 클래스 설계(게시글, 댓글)
    도메인 설계를 했다.
    기초적인 게시글과 댓글 표현이 가능하도록했다.
    DB와의 연동을 위한 추가적인 작업이 필요하다.

---
+++ src/main/java/com/example/boardproject/domain/ArticleComment.java
@@ -0,0 +1,15 @@
+package com.example.boardproject.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private Long id;
+    private Long articleId;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+}
