db8c0182ed97bfa7bb9eaca12d5c777690a466b8
hingjae <ljh23270@naver.com>
2023-04-25 12:25:34 +0900

    fixed untracked files

--- src/main/java/com/example/boardproject/domain/Article.java
+++ src/main/java/com/example/boardproject/domain/Article.java
@@ -1,17 +1,85 @@
 package com.example.boardproject.domain;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Entity
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+}) // 검색을 위해 인덱스 작업을 해줌. 본문검색은 용량이 너무 커서 지원하지 않음.
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //mysql id 생성전략 == identity
     private Long id;
 
+    @Setter
+    @Column(nullable = false)
     private String title;
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content;
+
+    @Setter
     private String hashtag;
 
+    //cascade => article이 사라지면 연관관계의 댓글도 사라짐.
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL) // mappedby안해주면 article_comment를 합쳐서 새로운 테이블을 만듦.
+    @ToString.Exclude //무한루프 방지
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+    protected Article() {
+    }
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    //팩토리 메서드
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag); //생성자 메서드 사용
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return getId() != null && getId().equals(article.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 }
