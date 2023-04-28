b83c2397030739f8380dafb3c43b01a99e0f3565
hingjae <ljh23270@naver.com>
2023-04-28 14:56:31 +0900

    #18 - 게시판 뷰 최소한의 html만 작성
    컨트롤러 메서드 articles가 통과하도록 테스트 작성

--- src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
+++ src/test/java/com/example/boardproject/controller/ArticleControllerTest.java
@@ -20,14 +20,13 @@ class ArticleControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 게시판 페이지 - 정상 호출")
     @Test
     void givenNothing_whenArticlesView_thenReturArticlesView() throws Exception {
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
-                .andExpect(view().name("articles/detail"))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
     }
 
@@ -37,7 +36,7 @@ class ArticleControllerTest {
     void givenNothing_whenArticleView_thenReturArticlesView() throws Exception {
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
