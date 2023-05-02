5df7dd57527680e5641a96d6a8a1093d785340f3
hingjae <ljh23270@naver.com>
2023-05-02 22:19:10 +0900

    댓글 저장 수정 삭제 메서드 구현

--- src/main/java/com/example/boardproject/service/ArticleCommentService.java
+++ src/main/java/com/example/boardproject/service/ArticleCommentService.java
@@ -1,15 +1,20 @@
 package com.example.boardproject.service;
 
+import com.example.boardproject.domain.Article;
 import com.example.boardproject.domain.ArticleComment;
 import com.example.boardproject.dto.ArticleCommentDto;
 import com.example.boardproject.repository.ArticleCommentRepository;
 import com.example.boardproject.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -21,6 +26,27 @@ public class ArticleCommentService {
     @Transactional(readOnly = true)
     public List<ArticleCommentDto> searchArticleComment(Long articleId) {
         return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+        try {
+            articleCommentRepository.save(dto.toEntity(articleRepository.getReferenceById(dto.articleId())));
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
+    }
+
+    public void updateArticleComment(ArticleCommentDto dto) {
+        try {
+            ArticleComment articleComment = articleCommentRepository.getReferenceById(dto.id());
+            if(dto.content() != null) {articleComment.setContent(dto.content());}
+        } catch (EntityNotFoundException e) {
+            log.warn("댓글 저장 실패. 댓글의 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
+    }
+
+    public void deleteArticleComment(Long articleCommentId) {
+        articleCommentRepository.deleteById(articleCommentId);
     }
 
 }
