2c4956e41d2c92721244c6b532076c4271de8707
hingjae <ljh23270@naver.com>
2023-05-09 19:15:24 +0900

    #47-해시태그 메서드 설정

--- src/main/java/com/example/boardproject/controller/ArticleController.java
+++ src/main/java/com/example/boardproject/controller/ArticleController.java
@@ -43,6 +43,7 @@ public class ArticleController {
         map.addAttribute("articles", articles);
         map.addAttribute("paginationBarNumbers", barNumbers);
         map.addAttribute("searchTypes", SearchType.values());
+        map.addAttribute("searchTypeHashtag", SearchType.HASHTAG);
 
         return "articles/index";
     }
@@ -52,7 +53,8 @@ public class ArticleController {
         ArticleWithCommentsResponse article = ArticleWithCommentsResponse.from(articleService.getArticleWithComments(articleId));
         map.addAttribute("article", article);
         map.addAttribute("articleComments", article.articleCommentsResponse());
-
+        map.addAttribute("totalCount", articleService.getArticleCount());
+        map.addAttribute("searchTypeHashtag", SearchType.HASHTAG);
         return "articles/detail";
     }
 
