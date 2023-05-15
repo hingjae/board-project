aa50bd15e8ac57b6fa6e42f2a7ad9f1501dbb17d
hingjae <ljh23270@naver.com>
2023-05-15 13:31:17 +0900

    카카오 oAuth 인증 기능 추가

--- src/main/java/com/example/boardproject/dto/security/BoardPrincipal.java
+++ src/main/java/com/example/boardproject/dto/security/BoardPrincipal.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,10 +18,15 @@ public record BoardPrincipal(
         Collection<? extends GrantedAuthority> authorities,
         String email,
         String nickname,
-        String memo
-) implements UserDetails {
+        String memo,
+        Map<String, Object> oAuth2Attributes
+) implements UserDetails, OAuth2User {
 
     public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
+        return BoardPrincipal.of(username, password, email, nickname, memo, Map.of());
+    }
+
+    public static BoardPrincipal of(String username, String password, String email, String nickname, String memo, Map<String, Object>oAuth2Attributes) {
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
         return new BoardPrincipal(
                 username,
@@ -30,7 +37,8 @@ public record BoardPrincipal(
                         .collect(Collectors.toUnmodifiableSet()), // 권한
                 email,
                 nickname,
-                memo
+                memo,
+                oAuth2Attributes
         );
     }
 
@@ -62,6 +70,12 @@ public record BoardPrincipal(
     @Override public boolean isAccountNonLocked() {return true;}
     @Override public boolean isCredentialsNonExpired() {return true;}
     @Override public boolean isEnabled() {return true;}
+
+    @Override
+    public Map<String, Object> getAttributes() { return oAuth2Attributes; } // 인증정보를 받았을 때 인증정보의 전체 내용이 이 곳에 있어야함.
+
+    @Override
+    public String getName() {return username; }
 
     public enum RoleType {
         USER("ROLE_USER");
