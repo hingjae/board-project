316c41326bc8cccd775049c81b2226594a6b3e74
hingjae <ljh23270@naver.com>
2023-06-08 17:05:47 +0900

    swagger ui 의존성 추가 및 간단 테스트

--- src/main/java/com/example/boardproject/controller/MainController.java
+++ src/main/java/com/example/boardproject/controller/MainController.java
@@ -1,7 +1,11 @@
 package com.example.boardproject.controller;
 
+import com.example.boardproject.dto.response.ArticleCommentResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class MainController {
@@ -11,4 +15,16 @@ public class MainController {
         return "forward:/articles";
     }
 
+    @ResponseBody
+    @GetMapping("/test-rest")
+    public ArticleCommentResponse test(Long id) {
+        return ArticleCommentResponse.of(
+                id,
+                "content",
+                LocalDateTime.now(),
+                "email@email.com",
+                "honey",
+                "honey"
+        );
+    }
 }
